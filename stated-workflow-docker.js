0a35e5a03ea0f11e3ada99f07e22544667f937b9
Sergey Sergeev <sergey.sergeev@appdynamics.com>
2024-01-09 18:58:55 -0800

    docker build

---
+++ stated-workflow-docker.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node --experimental-vm-modules
+import StatedREPL from 'stated-js/dist/src/StatedREPL.js'
+import TemplateProcessor from 'stated-js/dist/src/TemplateProcessor.js'
+import {StatedWorkflow} from "./src/workflow/StatedWorkflow.js";
+(async () => {
+    //starts a single-user REPL session in its own dedicated process therefore replacing
+    //the static DEFAULT_FUNCTIONS won't have side effects
+    TemplateProcessor.DEFAULT_FUNCTIONS = {...TemplateProcessor.DEFAULT_FUNCTIONS, ...StatedWorkflow.FUNCTIONS};
+    const tp = new TemplateProcessor();
+    tp.functionGenerators.set("serialGenerator", StatedWorkflow.serialGenerator);
+    const repl = new StatedREPL();
+    await repl.initialize();
+})();
