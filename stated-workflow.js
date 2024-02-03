4282b46c522ba174e0b8076414c79a4444347818
Sergey Sergeev <sergey.sergeev@appdynamics.com>
2024-02-02 21:36:08 -0800

    temporary workaround for resetting WorkflowDispatcher, yarn.lock++

--- stated-workflow.js
+++ stated-workflow.js
@@ -2,10 +2,13 @@
 import StatedREPL from 'stated-js/dist/src/StatedREPL.js'
 import TemplateProcessor from 'stated-js/dist/src/TemplateProcessor.js'
 import {StatedWorkflow} from "./src/workflow/StatedWorkflow.js";
+import {WorkflowDispatcher} from "./src/workflow/WorkflowDispatcher.js";
 (async () => {
     //starts a single-user REPL session in its own dedicated process therefore replacing
     //the static DEFAULT_FUNCTIONS won't have side effects
     const {templateProcessor:tp} = await StatedWorkflow.newWorkflow()
     const repl = new StatedREPL(tp);
+    // FIXME: This is a workaround and probably should better be set in StatedWorkflow.newWorkflow()
+    repl.cliCore.onInit = WorkflowDispatcher.clear;
     await repl.initialize();
 })();
